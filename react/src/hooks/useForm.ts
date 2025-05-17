938dcd3b0fff44a9ab9d70e946f9e42da8d4c536
Deivid Velkov <deivid0444d@gmail.com>
2025-05-17 19:07:10 +0300

    Update useForm.ts

--- react/src/hooks/useForm.ts
+++ react/src/hooks/useForm.ts
@@ -4,9 +4,19 @@ export interface LoginForm {
     email: string,
     password: string
 }
-export default function useForm(initialValue: LoginForm):
-    [LoginForm, (e: React.ChangeEvent<HTMLInputElement>) => void] {
-    const [formValue, setFormValue] = useState<LoginForm>(initialValue)
+export interface RegisterForm {
+    email: string,
+    profileIcon: string,
+    username: string,
+    number: number,
+    password: string,
+    rePassword: string
+}
+
+
+export default function useForm<T>(initialValue: T):
+    [T, (e: React.ChangeEvent<HTMLInputElement>) => void] {
+    const [formValue, setFormValue] = useState<T>(initialValue)
 
     const onChangeFormValue = (e: React.ChangeEvent<HTMLInputElement>) => {
         setFormValue(prev => ({ ...prev, [e.target.name]: e.target.value }))
