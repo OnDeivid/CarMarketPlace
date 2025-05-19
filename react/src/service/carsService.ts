66feab49beacf32aebd21a632d24cde53f1019db
Deivid Velkov <deivid0444d@gmail.com>
2025-05-19 17:47:58 +0300

    add Create functionality

--- react/src/service/carsService.ts
+++ react/src/service/carsService.ts
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { CreateAndEditForm } from "../hooks/useForm"
 
 export interface CarsInfo {
     brand: string,
@@ -19,4 +20,16 @@ export interface CarsInfo {
 export const onGetCars = async (): Promise<CarsInfo[]> => {
     const response = await axios.get('http://localhost:3000/data/home')
     return response.data
+}
+export const onCreate = async (formValue: CreateAndEditForm, userId: string, phoneNumber: string | number): Promise<any> => {
+    try {
+        const fullData = { ...formValue, userId, phoneNumber }
+        console.log(fullData)
+        const response = await axios.post('http://localhost:3000/data/create', fullData);
+        console.log(response)
+        return 'success'
+    } catch (error: any) {
+        const message = error?.response?.data?.error || 'Login failed. Please try again.';
+        return Promise.reject(new Error(message));
+    }
 }
